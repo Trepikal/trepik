1f60c14745a421cda845a5ccbd2a81d7fff2e2fb
Jack Ryan <jack.dixon.ryan@gmail.com>
2020-12-29 14:39:09 -0700

    The tutorials were fun but we've got a good product ux map now and the architecture has to change. So, it will. This will be a breaking change.'

--- trepik/lib/main.dart
+++ trepik/lib/main.dart
@@ -1,7 +1,6 @@
 import "package:flutter/material.dart";
 import "package:trepik/pages/home.dart";
 import "package:trepik/pages/loading.dart";
-import "package:trepik/pages/choose_location.dart";
 import "package:trepik/pages/profile.dart";
 import "package:trepik/pages/trek-page.dart";
 
@@ -10,7 +9,6 @@ void main() => runApp(MaterialApp(
   routes: {
     '/': (context) => Loading(),
     '/home': (context) => Home(),
-    '/location': (context) => ChooseLocation(),
     '/treks': (context) => Treks(),
     '/profile': (context) => Profile(),
   }
